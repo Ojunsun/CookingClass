8935dde7e965e9f9024839ff0190328fd9169ab3
Ojunsun <98933734+Ojunsun@users.noreply.github.com>
2024-11-06 09:34:10 +0900

    승리 UI 코드
    승리 UI 코드

--- Meracano/Assets/01_Scripts/UI/PopupUI.cs
+++ Meracano/Assets/01_Scripts/UI/PopupUI.cs
@@ -1,18 +1,49 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PopupUI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("PopupSetting")]
+    [SerializeField]
+    protected float _panelFadeTime;
+    [SerializeField]
+    protected float _panelDelayTime;
+
+    protected CanvasGroup _panel;
+
+    private Coroutine _panelOpenCoroutine = null;
+
+    public virtual void Awake()
     {
-        
+        _panel = GetComponent<CanvasGroup>();
+
+        if (_panel != null)
+        {
+            _panel.alpha = 0;
+            _panel.blocksRaycasts = false;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public virtual void OpenPopupUI()
     {
-        
+        if( _panelOpenCoroutine != null)
+            StopCoroutine(_panelOpenCoroutine);
+
+        _panelOpenCoroutine = StartCoroutine(ShowPanelCoroutine(_panelDelayTime));
+    }
+
+    IEnumerator ShowPanelCoroutine(float delayTime)
+    {
+        yield return new WaitForSeconds(delayTime);
+        _panel.blocksRaycasts = true;
+        _panel.DOFade(1, _panelFadeTime).SetUpdate(true).SetEase(Ease.InSine);
+    }
+
+    public virtual void ClosePopupUI()
+    {
+        _panel.blocksRaycasts = false;
+        _panel.DOFade(0, _panelFadeTime).SetUpdate(true);
     }
 }
--- Meracano/Assets/01_Scripts/UI/VictoryUI.cs
+++ Meracano/Assets/01_Scripts/UI/VictoryUI.cs
@@ -1,8 +1,27 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class VictoryUI : MonoBehaviour
+public class VictoryUI : PopupUI
 {
-    
+    public override void Awake()
+    {
+        EventManager.OnVictoryEvent += OnVictoryUIHandler;
+    }
+
+    private void OnVictoryUIHandler()
+    {
+        OpenPopupUI();
+    }
+
+    public override void OpenPopupUI()
+    {
+        base.OpenPopupUI();
+    }
+
+    public override void ClosePopupUI()
+    {
+        base.ClosePopupUI();
+    }
 }
